2c9b4f1d18276c987a7513d91d4c71a73b1cc517
Vinodbiradar09 <vinodjb07@gmail.com>
2025-12-07 22:53:50 +0530

    debounce 500

--- frontend/components/SearchBar.tsx
+++ frontend/components/SearchBar.tsx
@@ -17,7 +17,7 @@ export default function SearchBar({
   placeholder = 'Name, Phone number',
 }: SearchBarProps) {
   const [searchValue, setSearchValue] = useState(value);
-  const debouncedSearch = useDebounceCallback(onSearch, 700);
+  const debouncedSearch = useDebounceCallback(onSearch, 500);
   useEffect(() => {
     setSearchValue(value);
   }, [value]);
